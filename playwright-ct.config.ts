ccf4b799086ff34a1781367faaf26f244fb3c226
Mictim <mictim2002@gmail.com>
2023-04-06 22:23:54 +0200

    Containerization

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -16,9 +16,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 5 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+        ? [['dot'], ['allure-playwright']]
+        : [['list'], ['html', { open: 'on-failure' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
